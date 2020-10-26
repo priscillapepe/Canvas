e226ab588fd780c7be75d0dc310ef68b19f4c96b
priscillapepe <priscillaungai99@gmail.com>
2020-10-26 23:00:40 +0300

    updated file

--- avi/views.py
+++ avi/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django .http import HttpResponse
+from.models import post
+from .forms import NewProjectForm
 
 # Create your views here.
+
+
+def home(request):
+    context={
+        'posts':post.objects.all()
+        }
+    return render(request, 'avi/home.html',context)
+
+def about(request):
+     return render(request, 'avi/about.html',)
+
+def new_project(request):
+
+
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.editor = current_user
+            project.save()
+        return redirect('avi-home')
+
+    else:
+        form = NewProjectForm()
+    return render(request, 'new_project.html', {"form": form})
