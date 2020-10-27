6643a6e213b692bc5d36d4b71150d957d9507921
priscillapepe <priscillaungai99@gmail.com>
2020-10-27 09:20:08 +0300

    modified file

--- awards/settings.py
+++ awards/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tinymce',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -81,10 +82,13 @@ WSGI_APPLICATION = 'awards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME':'awards',
+           'USER': 'moringa',
+           'PASSWORD': 'Access',
+       }
     }
-}
+
 
 
 # Password validation
