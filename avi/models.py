c709636e1b07e985c1bb427539700cea376e8d64
priscillapepe <priscillaungai99@gmail.com>
2020-10-27 08:44:46 +0300

    updated file

--- avi/models.py
+++ avi/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from tinymce.models import HTMLField
 from django.contrib.auth.models import User
 
-class Project:
+class Project(models.Model):
     title = models.CharField(max_length=30)
-    image = models.ImageField(upload_to=new_projects)
+    image = models.ImageField(upload_to='new_project/')
     description = models.TextField(max_length=200)
     link = models.URLField(max_length=200)
     date = models.DateField(auto_now_add=True)
-    editor =models.ForeignKey(User,on_delete=on_delete.CASCADE)
+    editor =models.ForeignKey(User,on_delete=models.CASCADE, default='')
 
-def __str__(self):
-    return self.title
+    def save(self):
+        super().save()
+
